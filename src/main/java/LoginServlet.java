5134f06fc03faa5f40c1196377e8301f936f338b
Rick Morales <rick.morales7693@gmail.com>
2023-08-31 09:03:06 -0500

    added Profile and Logout Servlets and added WEB-INF directory

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        if (session != null && session.getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+
+        // Replace this block with real authentication logic
+        boolean validAttempt = authenticate(username, password);
 
         if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            response.sendRedirect("/WEB-INF/login.jsp");
         }
+    }
+
+    private boolean authenticate(String username, String password) {
+        // Implement your authentication logic here, e.g., database checks.
+        return username.equals("admin") && password.equals("password");
     }
 }
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -10,8 +10,9 @@
 <jsp:include page="partials/navbar.jsp"/>
 
 <div class="container">
-    <h1>Viewing your profile.</h1>
+    <h1>Viewing your profile.<%= session.getAttribute("user") %></h1>
 </div>
+<a href="/logout">Logout</a>
 
 </body>
 </html>
